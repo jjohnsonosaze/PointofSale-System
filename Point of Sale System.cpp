621a24ea776c0bf41302484ae180eb106a000c72
Chidozirim112 <130237003+Chidozirim112@users.noreply.github.com>
2023-05-10 12:26:16 +0100

    Update Point of Sale System.cpp

--- Point of Sale System.cpp
+++ Point of Sale System.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Product class to store the details of all products in Group 3 Stores
+// Product class to store the details of all products in Group 3 Stores ---
 class Product {
 private:
     string name;
